ef1cb70fb4d788630dc37dcf7228b35a9f95a16b
Jerrylejer <jerrylejer23@yahoo.fr>
2024-03-14 14:38:40 +0100

    Seconde requête

--- exercices.sql
+++ exercices.sql
@@ -1,7 +1,7 @@
 --1. Liste des potions : Numéro, libellé, formule et constituant principal. (5 lignes)
 select * from potion;
 --2. Liste des noms des trophées rapportant 3 points. (2 lignes)
-
+select nom_categ from categorie where nb_points = 3; 
 --3. Liste des villages (noms) contenant plus de 35 huttes. (4 lignes)
 select nom_village from village where nb_huttes > 35;
 --4. Liste des trophées (numéros) pris en mai / juin 52. (4 lignes)

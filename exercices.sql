545aa3fdb84cf49dbc79317121055708874cddd3
Jerrylejer <jerrylejer23@yahoo.fr>
2024-03-23 15:45:18 +0100

    Seizième requête, modification de l'intitulé des colonnes

--- exercices.sql
+++ exercices.sql
@@ -47,7 +47,7 @@ select nom as Habitants_sans_qualité from habitant where num_qualite is null;
 select nom as habitants_Potion_1_fevrier_52 from habitant join absorber on habitant.num_hab = absorber.num_hab where absorber.num_potion = 1 and absorber.date_a between '2052-02-01' and '2052-02-28';
 
 --16. Nom et âge des habitants par ordre alphabétique. (22 lignes)
-select nom, age from habitant order by nom;
+select nom as noms, age as ages from habitant order by nom;
 
 --17. Liste des resserres classées de la plus grande à la plus petite : nom de resserre et nom du village. (3 lignes)
 select nom_resserre as resserre, village.nom_village as Village from resserre join village on resserre.num_village = village.num_village order by resserre.superficie;
